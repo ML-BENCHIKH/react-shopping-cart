2fe5b5e9323a776f1973f153453614b3abaf3add
BENCHIKH Mohamed lamine <benchikh.med.lamine@gmail.com>
2021-12-30 23:06:33 +0100

    footer component

---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../../css/Footer/Footer.css"
+export default function Footer() {
+    return (
+        <footer>
+            &copy; CopyRights BENCHIKH Mohamed lamine 2022
+        </footer>
+    )
+}
